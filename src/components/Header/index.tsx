939281701d7055e2a0b88edf3f12251f06fccd99
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-09-12 13:14:22 +0200

    adding styles to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
 'use client'
 import Image from "next/image"
-import Logo from '../../../public/logo.png'
+import Logo from '../../../public/my-recipes-logo.png'
 
 const Header = () => {
    
     return (
         <header className="text-center  bg-amber-200">
-           <Image src={Logo} alt="logo" />   
+            <div className="flex justify-center">
+           <Image src={Logo} alt="logo" width={0} height={0} className="h-[auto] w-[50%] md:w-[25%] lg:w-[20%]"/> 
+           </div>  
         </header>
     )
     
--- src/components/LogInForm/index.tsx
+++ src/components/LogInForm/index.tsx
@@ -21,24 +21,24 @@ const LogInForm = () => {
         }    
     }
 
-    if(user) console.log("user is : " + user.name)
-
     const handleChange = (event: {target:{value:any}}) => {
         setUserInput(event.target.value)
     }
 
     return (
-        <form className="flex flex-col max-w-2xl rounded-2xl border-gray-900 border-2 m-auto p-4">
-            <h2 className="text-center font-bold text-2xl mb-4">LogIn!</h2>
-            <div className="flex">
-                <label htmlFor="username" className="m-2">Enter your username</label>
+        <form className="flex flex-col max-w-[400px] rounded-2xl border-amber-800 border-2 m-auto mt-8 shadow-2xl  p-4">
+            <h2 className="text-center font-bold text-3xl mb-4 text-[#FA824C] uppercase">LogIn!</h2>
+            <div className="flex justify-center m-4">
+                <label htmlFor="username" className="m-2">Username</label>
                 <input onChange={handleChange} id="username" placeholder="Username" value={userInput} className="m-2 border-2 rounded-xl border-gray-500"/>
             </div>
-            <div className="flex">
-                <label htmlFor="password" className="m-2">Enter your password</label>
+            <div className="flex justify-center m-4">
+                <label htmlFor="password" className="m-2">Password</label>
                 <input id="password" placeholder="password" className="m-2 border-2 rounded-xl border-gray-500"/>
             </div>
-            <button onClick={handleClick} className="border-2 border-gray-500 max-w-[100px] bg-gray-200">Log In !</button>
+            <div className="text-center">
+            <button onClick={handleClick} className="m-4 p-2 rounded-2xl cursor-pointer bg-amber-800 text-white">Log In !</button>
+            </div>
 
             {!userNotFound && <p>User Not found</p>}
         </form>
