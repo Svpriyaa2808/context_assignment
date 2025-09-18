e9ca52a6c6593d733bb6c3ef0ab07a190b8c9ea8
Priyadharshini Seetha Ram <svpriyaa2808@gmail.com>
2025-09-18 22:59:39 +0200

    fixing footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,21 +5,19 @@ import Navigation from "../Navigation"
 const Footer = () => {
     return (
         <footer className="bg-[#FCDFA6] mt-0 p-4">
-            <div className="flex justify-between">
-                <div className="flex justify-start flex-[40%]">
-                    <Image src={Logo} alt="logo" width={0} height={0} sizes="100vw" priority className="h-[auto] w-[75%] xs:w-[40%] sm:w-[35%] md:w-[25%] lg:w-[20%]"/> 
-                </div> 
+            <div className="flex flex-col md:flex-row justify-between">
+                <div className="text-center font-bold italic my-4 p-4 text-amber-800 text-[20px]">
+                Simple recipes made for real, actual, everyday life.
+            </div>
                
-                <div className="m-4 p-4 mr-6 text-[18px] ">
+                <div className="m-4 p-4 mr-6 text-[18px] text-center">
                     <p>About us</p>
                     <p>Privacy Policy</p>
                     <p>Blog</p>
                     <p>Contact</p>
                 </div>
             </div>
-            <div className="text-center font-bold italic my-4 text-amber-800 text-[20px]">
-                Simple recipes made for real, actual, everyday life.
-            </div>
+            
             <p className=" text-center">&copy; 2025.</p>
         </footer>
     )
